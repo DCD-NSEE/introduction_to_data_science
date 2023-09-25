b17a364c691bcebd9fe022f683c3dc2aa45f27b6
Pedro Guerra Judice Mesquita <21.02028-0@maua.br>
2023-09-25 10:21:57 -0300

    Correção do tezte.py

--- Pandas e Numpy/teste.py
+++ Pandas e Numpy/teste.py
@@ -10,28 +10,31 @@ class teste():
         self.resposta_1_5 = 4.08248290463863
         self.respostas_PYNB1 = {'1_1': self.resposta_1_1,'1_2_3': self.resposta_1_2_3,'1_5': self.resposta_1_5}
     
-    # def preparaPYNB2(self):
-    #     self.resposta_3_1 = "Hello world".find("r")
-    #     self.resposta_3_2 =  'The white spaces in this string must be replaced by underscores.'.replace(" ","_")
-    #     self.resposta_3_4 = {'SP': 45538936, 'RJ': 17159960, 'MG': 21040662, 'BA': 14812617}
-    #     self.resposta_3_5 = self.resposta_3_4['PR'] = 11348937
-    #     self.resposta_3_10 = [1, 0, 2, 4, 8, 1, 7, 1]
-    #     self.respostas_PYNB3 = {'3_1' : self.resposta_3_1,'3_2': self.resposta_3_2,'3_4': self.resposta_3_4,'3_5': self.resposta_3_5,'3_10': self.resposta_3_10}
+    def preparaPYNB2(self):
+        self.resposta_3_1 = "Hello world".find("r")
+        self.resposta_3_2 =  'The white spaces in this string must be replaced by underscores.'.replace(" ","_")
+        self.resposta_3_4 = {'SP': 45538936, 'RJ': 17159960, 'MG': 21040662, 'BA': 14812617}
+        self.resposta_3_5 = self.resposta_3_4['PR'] = 11348937
+        self.resposta_3_10 = [1, 0, 2, 4, 8, 1, 7, 1]
+        self.respostas_PYNB3 = {'3_1' : self.resposta_3_1,'3_2': self.resposta_3_2,'3_4': self.resposta_3_4,'3_5': self.resposta_3_5,'3_10': self.resposta_3_10}
 
-    # def preparaPYNB3(self):
-    #     self.resposta_4_2 = [7, 9, 5, 2, 0]
-    #     self.resposta_4_5a,self.resposta_4_5b,self.resposta_4_6a,self.resposta_4_5b = True
-    #     self.resposta_4_7 = 47
-    #     self.respostas_PYNB3 = {'4_2': self.resposta_4_2,'4_5a': self.resposta_4_5a,'4_5b': self.resposta_4_5b,'4_6a': self.resposta_4_6a,'4_5b': self.resposta_4_5b,'4_7': self.resposta_4_7}
+    def preparaPYNB3(self):
+        self.resposta_4_2 = [7, 9, 5, 2, 0]
+        self.resposta_4_5a = True
+        self.resposta_4_5b = True
+        self.resposta_4_6a = True
+        self.resposta_4_5b = True
+        self.resposta_4_7 = 47
+        self.respostas_PYNB3 = {'4_2': self.resposta_4_2,'4_5a': self.resposta_4_5a,'4_5b': self.resposta_4_5b,'4_6a': self.resposta_4_6a,'4_5b': self.resposta_4_5b,'4_7': self.resposta_4_7}
 
 
     def testa(self, nb, lista):
         if nb == 0:
             gabarito = self.respostas_PYNB1
-        # elif nb == 1:
-        #     gabarito = self.respostas_PYNB2
-        # elif nb == 2:
-        #     gabarito = self.respostas_PYNB3
+        elif nb == 1:
+            gabarito = self.respostas_PYNB2
+        elif nb == 2:
+            gabarito = self.respostas_PYNB3
 
 
         print("="*23)
